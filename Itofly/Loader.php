38cdf244ee2d6f3c6db0e48aaf53cfbfa15cd036
曹伟 <don_cao@sinobasedm.com>
2020-05-29 13:12:35 +0800

    去掉输出

--- Itofly/Loader.php
+++ Itofly/Loader.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace Itofly;
-class Loader{
-    public static function autoload($class){
-        var_dump($class);
-        require BASEDIR."/".str_replace('\\','/',$class).'.php';
+class Loader
+{
+    public static function autoload($class)
+    {
+        require BASEDIR . "/" . str_replace('\\', '/', $class) . '.php';
     }
 }
